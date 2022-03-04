d3860f94a896a6f8767096ef93c16507a8cbd85a
lmjh <lmjhemming@gmail.com>
2022-03-04 12:11:58 +0000

    Add confirmation modal to workout delete button

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.collapsible').collapsible();
     $('select').formSelect();
-    today = new Date();
     $('.datepicker').datepicker({format: "dd mmmm yy"});
     $('.timepicker').timepicker({defaultTime: "now"});
+    $('.modal').modal();
   });
