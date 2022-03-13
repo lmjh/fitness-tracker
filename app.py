e698bde352c2bf5519bc5d0bc77511f4dee4c799
lmjh <lmjhemming@gmail.com>
2022-03-13 17:25:36 +0000

    Build faq page.

--- app.py
+++ app.py
@@ -725,6 +725,15 @@ def getting_started():
                            page_title="Getting Started")
 
 
+@app.route("/faq")
+def faq():
+    """
+    Renders the faq page.
+    """
+    return render_template('faq.html',
+                           page_title="Frequently Asked Questions")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
